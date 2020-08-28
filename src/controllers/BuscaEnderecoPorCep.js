602be3e2b1bd1ba66671e67a7cf384b72910c7e4
Jack Wesley <jackwesley@hotmail.com>
2020-08-27 22:43:19 -0300

    Implementado a utilização do async await no Controller Busca Endereco Criação de funções para validação e limpeza do cep

--- src/controllers/BuscaEnderecoPorCep.js
+++ src/controllers/BuscaEnderecoPorCep.js
@@ -2,15 +2,39 @@ const CorreiosService = require('node-correios');
 const correioService = new CorreiosService();
 
 module.exports = {
-    index(request, response) {
+    async index(request, response) {
         const { cep } = request.body;
 
-        correioService.consultaCEP({ cep: cep })
-            .then(result => {
-                return response.json(result);
-            })
-            .catch(error => {
+
+        if (validaCep(cep)) {
+            const cepLimpo = removerCaracteresEspeciais(cep);
+            try {
+                const endereco = await correioService.consultaCEP({ cep: cepLimpo });
+
+                return response.json({ rua: endereco.logradouro, bairro: endereco.bairro, cidade: endereco.localidade, estado: endereco.uf });
+
+            } catch (error) {
                 return response.json(error);
-            });
+            }
+        }
+        else {
+            return response.status(400).json({ message: 'Cep invalido' });
+        }
+
+
     }
 };
+
+function removerCaracteresEspeciais(cep) {
+    const cepLimpo = cep.replace(/[^0-9]/g, '');
+    return cepLimpo;
+}
+
+function validaCep(cep) {
+
+    if (cep.length > 10) {
+        return false;
+    }
+
+    return true;
+}
