6f8d98f7dba799cb6d1fa1d10b98f6a9999b2a8b
Jack Wesley <jackwesley@hotmail.com>
2020-08-28 23:25:46 -0300

    Adicionando bibliotecas para acesso a banco de dados. Refatoraçao da BuscaEndereoController Criação da AuthController

--- src/controllers/BuscaEnderecoPorCep.js
+++ src/controllers/BuscaEnderecoPorCep.js
@@ -4,37 +4,68 @@ const correioService = new CorreiosService();
 module.exports = {
     async index(request, response) {
         const { cep } = request.body;
+        const cepLimpo = removePontoETraco(cep);
 
 
-        if (validaCep(cep)) {
-            const cepLimpo = removerCaracteresEspeciais(cep);
+        if (validaCep(cepLimpo)) {
             try {
-                const endereco = await correioService.consultaCEP({ cep: cepLimpo });
-
-                return response.json({ rua: endereco.logradouro, bairro: endereco.bairro, cidade: endereco.localidade, estado: endereco.uf });
-
+                var endereco = await buscaEndereco(cepLimpo, 7);
+                return response.json({ rua: endereco.logradouro, bairro: endereco.bairro, cidade: endereco.localidade, estado: endereco.uf })
             } catch (error) {
-                return response.json(error);
+                return response.status(400).json({ message: 'Cep inválido' });
             }
         }
-        else {
-            return response.status(400).json({ message: 'Cep invalido' });
-        }
 
-
+        return response.status(400).json({ message: 'Cep inválido' });
     }
 };
 
-function removerCaracteresEspeciais(cep) {
-    const cepLimpo = cep.replace(/[^0-9]/g, '');
+
+async function buscaEndereco(cep, indice) {
+    try {
+        const endereco = await correioService.consultaCEP({ cep: cep });
+        if (endereco.erro !== true) {
+            return endereco;
+        }
+        else {
+
+            if (indice >= 1) {
+                var cepCorrigido = adicionaZeroAoCepNaoEncontrado(cep, indice);
+                console.log(cepCorrigido)
+                indice = indice - 1;
+                return buscaEndereco(cepCorrigido, indice);
+            }
+        }
+    } catch (error) {
+        return error;
+    }
+}
+
+
+function adicionaZeroAoCepNaoEncontrado(cep, indice) {
+
+    if (cep[indice] === "0" && indice > 0) {
+        return adicionaZeroAoCepNaoEncontrado(cep, indice - 1);
+    }
+    else {
+        resultado = replaceAt(cep, indice, "0");
+        return resultado;
+    }
+}
+
+
+function replaceAt(str, index, chr) {
+    if (index > str.length - 1) return str;
+    return str.substring(0, index) + chr + str.substring(index + 1);
+}
+
+function removePontoETraco(cep) {
+    const cepLimpo = cep.replace(/\.|\-/g, '');
     return cepLimpo;
 }
 
 function validaCep(cep) {
+    var validaCep = /^[0-9]{8}$/;
 
-    if (cep.length > 10) {
-        return false;
-    }
-
-    return true;
+    return validaCep.test(cep);
 }
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const BuscaEndereco = require('./controllers/BuscaEnderecoPorCep');
-
+const AuthController = require('./controllers/AuthController');
 const routes = express.Router();
 
 routes.post('/busca-endereco', BuscaEndereco.index);
+routes.post('/authenticate', AuthController.index);
+
 
 
 module.exports = routes;
