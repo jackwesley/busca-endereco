0ce92207b6728557d3d7074cebb986dd2bcd19d8
Jack Wesley <jackwesley@hotmail.com>
2020-08-29 23:01:49 -0300

    Finalizado implementação da documentação por Swagger

--- src/routes.js
+++ src/routes.js
@@ -20,80 +20,184 @@ const routes = express.Router();
  *         type: string
  *       password:
  *         type: string
+  *   userAuthenticate:
+ *     properties:
+ *       email:
+ *         type: string
+ *       password:
+ *         type: string
  */
 
 /**
  * @swagger
  * /busca-endereco:
  *   post:
- *     description: Retorna o endereço de acordo com o CEP informado
+ *     summary: Busca um endereço.
+ *     consumes:
+ *       - application/json
+ *     produces:
+ *       - application/json
  *     parameters:
- *       - zipCode: CEP
- *         description: CEP a ser buscado
- *         in: body
- *         required: true
- *         type: string
- *       - x-access-token: x-access-token
+ *       - name: x-access-token
  *         description: Token para habilitar a busca
- *         in: head
+ *         in: header
  *         required: true
- *         type: string
+ *         type: string  
+ *       - name: body
+ *         in: body
+ *         schema:
+ *           type: object
+ *           properties:
+ *             zipCode:
+ *               type: string
+ *               required: true
+ *               example: "37503192"
+ *         required:
+ *           - body
  *     responses:
- *       200:
- *         description: Resposta de sucesso
+ *       '200':
+ *         description: Success.
+ *         schema:
+ *           type: object
+ *           properties:
+ *             message:
+ *               type: string
+ *               example: Success.
+ *       '401':
+ *         description: Invalid auth token provided.
+ *         schema:
+ *           type: object
+ *           properties:
+ *             message:
+ *               type: string
+ *               example: Invalid auth token provided.
+ *       '400':
+ *         description: Bad request.
+ *         schema:
+ *           type: object
+ *           properties:
+ *             message:
+ *               type: string
+ *               example: Bad request.
+ *       '404':
+ *         description: Not Found.
+ *         schema:
+ *           type: object
+ *           properties:
+ *             message:
+ *               type: string
+ *               example: Not Found.
  */
 routes.post('/busca-endereco', authService.authorize, adressController.index);
 
 /**
  * @swagger
  * /user:
- *  post:
- *    description: Este endpoint é responsável por cadastrar um usuário no sistema
- *    parameters:
- *       - name: firstName
- *         description: Primeiro nome
+ *   post:
+ *     summary: Cadastra usuário.
+ *     consumes:
+ *       - application/json
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: body
  *         in: body
- *         required: true
- *         type: string
- *       - name: lastName
- *         description: Ultimo Nome
- *         in: body
- *         required: true
- *         type: string
- *       - name: email
- *         description: Email
- *         in: body
- *         required: true
- *         type: string
- *       - name: password
- *         description: Senha
- *         in: body
- *         required: true
- *         type: string 
  *         schema:
- *           $ref: '#/definitions/user'
- *    responses: 
- *       '200':
- *        description: Retorno com sucesso
+ *           type: object
+ *           properties:
+ *             firstName:
+ *               type: string
+ *               required: true
+ *               example: John
+ *             lastName:
+ *               type: string
+ *               required: true
+ *               example: Wick
+ *             email:
+ *               type: string
+ *               required: true
+ *               example: meuemail@mail.com
+ *             password:
+ *               type: string
+ *               required: true
+ *               example: "12345678"
+ *         required:
+ *           - body
+ *     responses:
+ *       '201':
+ *         description: User created successfully.
+ *         schema:
+ *           type: object
+ *           properties:
+ *             message:
+ *               type: string
+ *               example: User created successfully.
  *       '400':
- *        description: Requisição má formatada
+ *         description: Bad Request.
+ *         schema:
+ *           type: object
+ *           properties:
+ *             message:
+ *               type: string
+ *               example: Bad Request.
  */
- 
 routes.post('/user', userController.create);
 
 /**
  * @swagger
  * /authenticate:
- *  post:
- *    description: Este endpoint é responsável por gerar o token de autenticação para o usuário já cadastrado
- *    responses: 
+ *   post:
+ *     summary: Gera token para usuário cadastrado.
+ *     consumes:
+ *       - application/json
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: body
+ *         in: body
+ *         schema:
+ *           type: object
+ *           properties:
+ *             email:
+ *               type: string
+ *               required: true
+ *               example: meuemail@mail.com
+ *             password:
+ *               type: string
+ *               required: true
+ *               example: "12345678"
+ *         required:
+ *           - body
+ *     responses:
  *       '200':
- *        description: Retorno com sucesso
+ *         description: Success.
+ *         schema:
+ *           type: object
+ *           properties:
+ *             message:
+ *               type: string
+ *               example: Success.
+ *       '400':
+ *         description: Bad Request.
+ *         schema:
+ *           type: object
+ *           properties:
+ *             message:
+ *               type: string
+ *               example: Bad Request.
+ *       '404':
+ *         description: Not found.
+ *         schema:
+ *           type: object
+ *           properties:
+ *             message:
+ *               type: string
+ *               example: Not found.
  */
 routes.post('/authenticate', authController.authenticacte);
 
 
-routes.get('/health',  (_, res) => res.sendStatus(200));
+routes.get('/health', (_, res) => res.sendStatus(200));
 
 
 module.exports = routes;
